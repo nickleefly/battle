1a29d28b6982302400bdf89538c49931f6c83e72
Xiuyu Li <nickleefly@gmail.com>
2017-06-20 15:22:51 +0800

    first Component

---
+++ app/index.js
@@ -0,0 +1,17 @@
+var React = require('react')
+var ReactDom = require('react-dom')
+require('./index.css')
+
+// state
+// lifecycle event
+// UI
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
