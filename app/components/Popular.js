60d758d7d0a129ce62847044ca7a3395910e5936
Xiuyu Li <nickleefly@gmail.com>
2017-06-22 14:26:53 +0800

    use function as Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,24 +1,22 @@
 var React = require('react')
 var PropTypes = require('prop-types')
 
-class SelectLanguage extends React.Component {
-  render () {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
-    return (
-      <ul className='languages'>
-        {languages.map(function (lang) {
-          return (
-            <li
-              style={lang === this.props.selectedLanguage ? {color: '#d0021b'} : null}
-              onClick={this.props.onSelect.bind(null, lang)}
-              key={lang}>
-              {lang}
-            </li>
-          )
-        }, this)}
-      </ul>
-    )
-  }
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+  return (
+    <ul className='languages'>
+      {languages.map(function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+            {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
 class Popular extends React.Component {
