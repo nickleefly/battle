488dabe84c0d60a4dde0a7836311a3ed75f31c35
Xiuyu Li <nickleefly@gmail.com>
2017-06-26 12:01:59 +0800

    props.children

--- app/components/battle.js
+++ app/components/battle.js
@@ -1,33 +1,7 @@
 var React = require('react')
 var PropTypes = require('prop-types')
 var Link = require('react-router-dom').Link
-
-function PlayerPreview (props) {
-  return (
-    <div>
-      <div className='column'>
-        <img
-          className='avatar'
-          src={props.avatar}
-          alt={'Avatar for ' + props.username}
-        />
-        <h2 className='username'>@{props.username}</h2>
-      </div>
-      <button
-        className='reset'
-        onClick={props.onReset.bind(null, props.id)}>
-          Reset
-      </button>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired
-}
+var PlayerPreview = require('./PlayerPreview')
 
 class PlayerInput extends React.Component {
   constructor (props) {
@@ -139,12 +113,17 @@ class Battle extends React.Component {
             />}
 
           {playerOneImage !== null &&
-            <PlayerPreview
-              avatar={playerOneImage}
-              username={playerOneName}
-              onReset={this.handleReset}
-              id='playerOne'
-            />}
+          <PlayerPreview
+            avatar={playerOneImage}
+            username={playerOneName}
+          >
+            <button
+              className='reset'
+              onClick={this.handleReset.bind(this, 'playerOne')}>
+                Reset
+              </button>
+          </PlayerPreview>
+          }
 
           {!playerTwoName &&
             <PlayerInput
@@ -157,9 +136,14 @@ class Battle extends React.Component {
           <PlayerPreview
             avatar={playerTwoImage}
             username={playerTwoName}
-            onReset={this.handleReset}
-            id='playerTwo'
-            />}
+            >
+            <button
+              className='reset'
+              onClick={this.handleReset.bind(this, 'playerTwo')}>
+                Reset
+            </button>
+          </PlayerPreview>
+          }
 
         </div>
         {playerOneImage && playerTwoImage &&
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,36 +1,36 @@
 var axios = require('axios')
 
-var id = "5f5d02b41bd203363272";
-var sec = "9448ae06473e6bce88e9fa9ae9c5824fb598eab7";
-var params = "?client_id=" + id + "&client_secret=" + sec;
+var id = '5f5d02b41bd203363272'
+var sec = '9448ae06473e6bce88e9fa9ae9c5824fb598eab7'
+var params = '?client_id=' + id + '&client_secret=' + sec
 
 function getProfile (username) {
   return axios.get('https://api.github.com/users/' + username + params)
     .then(function (user) {
-      return user.data;
-    });
+      return user.data
+    })
 }
 
 function getRepos (username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
 }
 
 function getStarCount (repos) {
   return repos.data.reduce(function (count, repo) {
     return count + repo.stargazers_count
-  }, 0);
+  }, 0)
 }
 
 function calculateScore (profile, repos) {
-  var followers = profile.followers;
-  var totalStars = getStarCount(repos);
+  var followers = profile.followers
+  var totalStars = getStarCount(repos)
 
-  return (followers * 3) + totalStars;
+  return (followers * 3) + totalStars
 }
 
 function handleError (error) {
-  console.warn(error);
-  return null;
+  console.warn(error)
+  return null
 }
 
 function getUserData (player) {
@@ -38,27 +38,27 @@ function getUserData (player) {
     getProfile(player),
     getRepos(player)
   ]).then(function (data) {
-    var profile = data[0];
-    var repos = data[1];
+    var profile = data[0]
+    var repos = data[1]
 
     return {
       profile: profile,
       score: calculateScore(profile, repos)
     }
-  });
+  })
 }
 
 function sortPlayers (players) {
-  return players.sort(function (a,b) {
-    return b.score - a.score;
-  });
+  return players.sort(function (a, b) {
+    return b.score - a.score
+  })
 }
 
 module.exports = {
   battle: function (players) {
     return axios.all(players.map(getUserData))
       .then(sortPlayers)
-      .catch(handleError);
+      .catch(handleError)
   },
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories')
