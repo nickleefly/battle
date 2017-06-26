17f7ef8afb37891db6cd31f94f2631564dfe39fb
Xiuyu Li <nickleefly@gmail.com>
2017-06-26 14:26:45 +0800

    webpack config change, publish to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
+    filename: 'index_bundle.js'
   },
   module: {
     rules: [
@@ -22,4 +21,15 @@ module.exports = {
       template: 'app/index.html'
     })
   ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
 }
